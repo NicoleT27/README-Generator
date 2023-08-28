3c8d88673485deb4ae0285c39d485667da38a4ba
Nicole <nicolejoan9@yahoo.com>
2023-08-28 09:38:09 -0400

    updated file path for index.js to work

--- README.md
+++
@@ -1,2 +0,0 @@
-# README-Generator
-A application that dynamically generates a professional README.md file with user answers via the command line
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 // TODO: Include packages needed for this application
-const fs = require('fs');
-const inquirer = require('inquirer');
-// const utils = require('utils');
-const generateMarkdown = require('./utils/generateMarkdown');
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -19,28 +18,34 @@ const questions = [
   {
     type: "input",
     name: "Install",
-    message: "What does the user need to install to be able to run your application.",
+    message:
+      "What does the user need to install to be able to run your application.",
   },
   {
     type: "input",
     name: "Usage",
-    message: "Please explain how the application is used via instructions.",
+    message: "Please give insturctions on how to utilize your application.",
   },
   {
     type: "input",
     name: "License",
-    message: "Please select a license for your application",
-    choices : ["BSD", " MIT","GNU GPL 3.0","Apache 2.0","ISC","none"]
+    message:
+      "Please select a license for your application from the following : BSD, MIT, GNU GPL 3.0, Apache 2.0, ISC, or none.",
   },
   {
     type: "input",
     name: "Contributers",
-    message: "Did you have anyone contribute? If so, please name them.",
+    message: "Did you have any contributors? If so, please name them.",
   },
   {
     type: "input",
     name: "Test",
     message: "What commands does the user need to test the application?",
+  },
+  {
+    type: "input",
+    name: "Full Name",
+    message: "Please enter your full name.",
   },
   {
     type: "input",
@@ -50,37 +55,29 @@ const questions = [
   {
     type: "input",
     name: "Email",
-    message: "Please enter your email address.",
-  },
-  {
-    type: "input",
-    name: "Full Name",
-    message: "Please enter your full name.",
+    message: "Lastly please enter your email address.",
   },
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, data, (err) => {
-      console.log(fileName);
-      console.log(data);
-      if (err) {
-        return console.log("There has been an error");
-      } else {
-        console.log("success");
-      }
-    });
+  fs.writeFile(fileName, data, (err) => {
+    console.log(fileName);
+    console.log(data);
+    if (err) {
+      return console.log("There has been an error");
+    } else {
+      console.log("success");
+    }
+  });
 }
 
 // TODO: Create a function to initialize app
 function init() {
-inquirer
-  .prompt(questions)
-  .then((data) => {
+  inquirer.prompt(questions).then((data) => {
     writeToFile("README.md", generateMarkdown(...data));
     console.log("Generating your professional README file");
-  })
-
+  });
 }
 // Function call to initialize app
 init();
